c7100d2294f229425895bd7d891bce69bab26f62
Matheus Vitoriano <matheusbzv@gmail.com>
2020-02-14 09:07:34 -0300

    Attempt to solve Heroku Binding Issue

--- server.js
+++ server.js
@@ -37,5 +37,14 @@ app.get('*', function (req, res) {
   res.redirect('/');
 });
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+# port (as described above) and host are both wrong
+const host = 'localhost';
+const port = 3000;
+
+# use alternate localhost and the port Heroku assigns to $PORT
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+  console.log("Server started.......");
+});
