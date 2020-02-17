7c122c100fc0f3132ad46e9087a56da54e1b352e
Igor Theodoro <igortheodoro12@gmail.com>
2020-02-17 19:26:10 -0300

    Remove mailgun
    As políticas do módulo não permitem o que código do mesmo fique exposto em repositórios abertos

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express'),
   app = express(),
-  mailgun = require("mailgun-js"),
   bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-const DOMAIN = "sandbox5a7ce9437dd748c383f7d184c9a274cb.mailgun.org";
-const mg = mailgun({apiKey: "3ff3141a88e0bb92173989e6c87626e3-52b6835e-a79e8689", domain: DOMAIN});
 
 app.set('view engine', 'ejs')
 app.use(express.static('public')); //Usará a pasta public
@@ -17,15 +14,6 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) { 
-  const data = {
-    from: req.body.email,
-    to: "igortheodoro12@gmail.com",
-    subject: req.body.name,
-    text: req.body.content
-  };
-  mg.messages().send(data, function (error, body) {
-    console.log(body);
-  });
 
   res.redirect('/')
 })
